75ae62161c95374ffc149cba49329ac2a346d6aa
Fubuki <75804199+Srisuphadith@users.noreply.github.com>
2025-10-07 17:27:28 +0700

    login and register

--- src/sql.ts
+++ src/sql.ts
@@ -3,16 +3,16 @@ const connection = await mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "CPE241_SHOP",
+  database: "Student_planner",
   port: 3306,
 });
 
 export async function login_validation(username: string, pass: string) {
-  const sql_typing = "SELECT password_hash FROM tbl_users WHERE userName = ?"
+  const sql_typing = "SELECT hash FROM users WHERE username = ?"
   try {
     const [rows] = await connection.execute(sql_typing, [username]);
     if (rows.length === 1) {
-      if (await Bun.password.verify(pass, rows[0].password_hash)) {
+      if (await Bun.password.verify(pass, rows[0].hash)) {
         return "login successfully"
       }
     }
@@ -22,12 +22,12 @@ export async function login_validation(username: string, pass: string) {
     console.error("Error fetching users:", err);
   }
 }
-export async function create_user(firstname: string, lastname: string, username: string, password: string) {
-  const sql_typing = "INSERT INTO tbl_users (firstName,lastName,userName,password_hash) VALUES (?,?,?,?)"
-  const [rows_k] = await connection.execute("SELECT userName FROM tbl_users WHERE userName = ?", [username])
+export async function create_user(firstname: string, lastname: string, username: string, password: string,email:string) {
+  const sql_typing = "INSERT INTO users (firstname,lastname,username,hash,email) VALUES (?,?,?,?,?)"
+  const [rows_k] = await connection.execute("SELECT username FROM users WHERE username = ?", [username])
+
   if (rows_k.length != 1) {
-    console.log(rows_k)
-    const [rows] = await connection.execute(sql_typing, [firstname, lastname, username, await Bun.password.hash(password)])
+    const [rows] = await connection.execute(sql_typing, [firstname, lastname, username, await Bun.password.hash(password),email])
     if (rows.affectedRows === 1) {
       return "register successfully.";
     }
