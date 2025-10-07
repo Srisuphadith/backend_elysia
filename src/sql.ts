b9a41ca5f728126c2d612072e8ebde714d654114
Fubuki <75804199+Srisuphadith@users.noreply.github.com>
2025-10-07 18:02:05 +0700

    Update sql.ts

--- src/sql.ts
+++ src/sql.ts
@@ -7,25 +7,34 @@ const connection = await mysql.createConnection({
   port: 3306,
 });
 
+const message_empty = "please fill information."
+
 export async function signin(username: string, pass: string) {
   const sql_typing = "SELECT hash FROM users WHERE username = ?"
   try {
+    if (username === "" || pass === "") {
+      return message_empty
+    }
     const [rows] = await connection.execute(sql_typing, [username]);
     if (rows.length === 1) {
       if (await Bun.password.verify(pass, rows[0].hash)) {
         return "login successfully"
       }
     }
-    return "login faild"
+    return "login faild wrong username or plassword."
+
   } catch (err) {
     console.error("Error fetching users:", err);
     return err
   }
 }
-export async function register(firstname: string, lastname: string, username: string, password: string, email: string) {
+
+export async function register(firstname: string, lastname: string, username: string, password: string, email: string){
   const sql_insert_user = "INSERT INTO users (firstname,lastname,username,hash,email) VALUES (?,?,?,?,?)"
   const sql_check_user = "SELECT username FROM users WHERE username = ?"
-
+  if(is_empty(firstname,lastname,username,password,email)){
+    return message_empty
+  }
   const [rows_k] = await connection.execute(sql_check_user, [username])
   if (rows_k.length != 1) {
     const [rows] = await connection.execute(sql_insert_user, [firstname, lastname, username, await Bun.password.hash(password), email])
@@ -34,4 +43,9 @@ export async function register(firstname: string, lastname: string, username: st
     }
   }
   return "user alrady exits"
+}
+
+
+function is_empty(firstname: string, lastname: string, username: string, password: string, email: string){
+    return firstname === "" || lastname === "" || username === "" || password === "" || email === ""
 }
